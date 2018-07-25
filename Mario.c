3ae70d635f40ee85da3641fc67a4b72fb69a019f
WeeNch <32383962+weenchvd@users.noreply.github.com>
2018-07-25 11:22:18 +0300

    UPD

---
+++ Mario.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <cs50.h>
+
+
+void main()
+{
+    int H;
+    do
+    {
+       printf("The height of the Mario staircase (from 0 to 23):\n");
+       H = get_int();
+    } while(H < 0 || H > 23);
+    printf("------------------------------\n");
+    if(H == 0)
+    {
+        printf(" \n");
+    }
+    else
+    {
+        for(int y = H; y > 0; y--) // y = H - высота лестницы; цикл уменьшает количество строк в лестнице
+        {
+            for(int x = (H + 1), S = (y - 1); x > 0; S--, x--) // x = (H + 1) - ширина лестницы; уменьшает количество символов в строке
+            {
+                if(S > 0)
+                {
+                    printf(" ");
+                }
+                else
+                {
+                    printf("#");
+                }
+            }
+            printf("\n");
+        }
+    }
+    printf("------------------------------\n");
+}
