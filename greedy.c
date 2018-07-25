62b5d7c47d992da173e958c749dfddae4e39dd1b
WeeNch <32383962+weenchvd@users.noreply.github.com>
2018-07-25 15:44:26 +0300

    Initial commit
    (cherry picked from commit f8ec9d29aceaa1ac8fea1eba3b80406cb46b50dd)

--- greedy.c
+++ greedy.c
@@ -11,7 +11,7 @@ int main()
         printf("How much change is owed?\n");
         M = get_float();
     } while(M <= 0 || M > 1000);
-    double M2 = (M * 100);
+    double M2 = ((M + 0.002) * 100.0);
     int C = (int) M2;
 
 
@@ -22,6 +22,11 @@ int main()
     printf("%f\n", M);
     printf("%f\n", M2);
     printf("%d\n", C);
+    //printf("sizeof(M) = %d \n", sizeof(M));
+    //printf("sizeof(M2) = %d \n", sizeof(M2));
+    //printf("sizeof(C) = %d \n", sizeof(C));
+    //printf("INT_MIN = %d \n", INT_MIN);
+    //printf("INT_MAX = %d \n", INT_MAX);
 }
 
 
