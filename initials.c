a2cba04ec141ad6c73f0c8782f20e1f9b03e2f24
weenchvd <d88744@gmail.com>
2018-07-26 20:49:41 +0300

    UPD

--- initials.c
+++ initials.c
@@ -8,16 +8,16 @@ int main()
     string S = get_string();
     char ch1 = S[0];
     if(S[0] > 40 && S[0] < 91)
-            {
-                printf("%c", S[0]);
-            }
+    {
+        printf("%c", S[0]);
+    }
     else
-            {
-                int n = (int) S[0];
-                n -=32;
-                ch1 = (char) n;
-                printf("%c", ch1);
-            }
+    {
+        int n = (int) S[0];
+        n -=32;
+        ch1 = (char) n;
+        printf("%c", ch1);
+    }
 
     for(int i = 0; i < strlen(S); i++)
     {
---
+++ initials_2_2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include "Libs/libcs50-8.1.0/cs50.h"
+
+void PRINTLETTER(char ch1);
+void printletter(char ch1);
+
+char ch1;
+
+int main()
+{
+    string S = get_string();
+    ch1 = S[0];
+    (S[0] > 40 && S[0] < 91) ? PRINTLETTER(ch1) : printletter(ch1);
+    for(int i = 0; i < strlen(S); i++) if(S[i] == 32) (S[i + 1] > 40 && S[i + 1] < 91) ? (ch1 = S[i + 1], PRINTLETTER(ch1)) : (ch1 = S[i + 1], printletter(ch1));
+    printf("\n");
+}
+
+void PRINTLETTER(char ch1) //печатает прописную букву
+{
+    printf("%c", ch1);
+}
+
+void printletter(char ch1) //печатает прописную букву из строчной
+{
+    int n = (int) ch1;
+    n -=32;
+    ch1 = (char) n;
+    printf("%c", ch1);
+}
