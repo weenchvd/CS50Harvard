5b8066d5afd493a11b4a203b235d3dd5125a7f74
weenchvd <d88744@gmail.com>
2018-07-27 23:13:08 +0300

    UPD

--- Sourses/Vigenere.c
+++ Sourses/Vigenere.c
@@ -6,26 +6,47 @@
 void PrintEncLowLetter(char Letter);
 void PrintEncUppLetter(char Letter);
 void PrintEncDigit(char Letter);
+void CalcKeyLenth(void);
 
-int KeyLenth;
+int KeyLenth, n1;
+int t = 0;
+string KeyWord;
 
 int main(int argc, string argv[])
 {
     if (argc != 2) return 1;
-    string KeyWord = argv[1];
+    KeyWord = argv[1];
     for (int i = 0, n1 = strlen(KeyWord); i < n1; i++)
     {
-        if (isalpha(KeyWord[i]));
-        else return 1;
+        if (isalpha(KeyWord[i]) != 1) return 1;
     }
     string InputText = get_string();
     for(int i = 0, n2 = strlen(InputText); i < n2; i++)
     {
-        if(islower(InputText[i])) PrintEncLowLetter(InputText[i]);
-        else if(isupper(InputText[i])) PrintEncUppLetter(InputText[i]);
-        else if(isdigit(InputText[i])) PrintEncDigit(InputText[i]);
+        if(islower(InputText[i]))
+        {
+            CalcKeyLenth();
+            PrintEncLowLetter(InputText[i]);
+        }
+        else if(isupper(InputText[i]))
+        {
+            CalcKeyLenth();
+            PrintEncUppLetter(InputText[i]);
+        }
+        else if(isdigit(InputText[i]))
+        {
+            CalcKeyLenth();
+            PrintEncDigit(InputText[i]);
+        }
         else printf("%c", InputText[i]);
     }
+    return 0;
+}
+
+void CalcKeyLenth(void)
+{
+    KeyLenth = (int) KeyWord[t];
+    (t < n1) ? t++ : (t = 0);
 }
 
 void PrintEncLowLetter(char Letter)
