0f161fedc36951a1d914bacb6a96cf52eb8dff76
WeeNch <spam88744@gmail.com>
2018-07-28 22:43:36 +0300

    UPDH

--- Sourses/Vigenere.c
+++ Sourses/Vigenere.c
@@ -6,7 +6,6 @@
 void PrintEncLowLetter(char Letter);
 void PrintEncUppLetter(char Letter);
 void PrintEncDigit(char Letter);
-void CalcKeyLenth(void);
 
 int KeyLenth, n1;
 int t = 0;
@@ -16,42 +15,29 @@ int main(int argc, string argv[])
 {
     if (argc != 2) return 1;
     KeyWord = argv[1];
-    for (int i = 0, n1 = strlen(KeyWord); i < n1; i++)
+    n1 = strlen(KeyWord);
+    for (int i = 0; i < n1; i++)
     {
         if (isalpha(KeyWord[i]) != 1) return 1;
     }
     string InputText = get_string();
     for(int i = 0, n2 = strlen(InputText); i < n2; i++)
     {
-        if(islower(InputText[i]))
-        {
-            CalcKeyLenth();
-            PrintEncLowLetter(InputText[i]);
-        }
-        else if(isupper(InputText[i]))
-        {
-            CalcKeyLenth();
-            PrintEncUppLetter(InputText[i]);
-        }
-        else if(isdigit(InputText[i]))
-        {
-            CalcKeyLenth();
-            PrintEncDigit(InputText[i]);
-        }
+        //printf("t=%d, n1=%d, KeyLenth=%d, KeyWord=%s\t", t, n1, KeyLenth, KeyWord);
+        if(islower(InputText[i])) PrintEncLowLetter(InputText[i]);
+        else if(isupper(InputText[i])) PrintEncUppLetter(InputText[i]);
+        else if(isdigit(InputText[i])) PrintEncDigit(InputText[i]);
         else printf("%c", InputText[i]);
+        //printf("\t t=%d, n1=%d, KeyLenth=%d, KeyWord=%s\n", t, n1, KeyLenth, KeyWord);
     }
     return 0;
 }
 
-void CalcKeyLenth(void)
-{
-    KeyLenth = (int) KeyWord[t];
-    (t < n1) ? t++ : (t = 0);
-}
-
 void PrintEncLowLetter(char Letter)
 {
-    int NumLetter = (int) Letter;
+    KeyLenth = ((int) KeyWord[t]) - 97; //Calculate KeyLenth
+    (t < (n1 - 1)) ? t++ : (t = 0);
+    int NumLetter = (int) Letter; //Calculate EncLowLetter
     NumLetter += KeyLenth;
     if(NumLetter > 122) NumLetter = (NumLetter % 122) + 96;
     printf("%c", NumLetter);
@@ -59,7 +45,9 @@ void PrintEncLowLetter(char Letter)
 
 void PrintEncUppLetter(char Letter)
 {
-    int NumLetter = (int) Letter;
+    KeyLenth = ((int) KeyWord[t]) - 65; //Calculate KeyLenth
+    (t < (n1 - 1)) ? t++ : (t = 0);
+    int NumLetter = (int) Letter; //Calculate EncUppLetter
     NumLetter += KeyLenth;
     if(NumLetter > 90) NumLetter = (NumLetter % 90) + 64;
     printf("%c", NumLetter);
@@ -67,7 +55,9 @@ void PrintEncUppLetter(char Letter)
 
 void PrintEncDigit(char Letter)
 {
-    int NumLetter = (int) Letter;
+    KeyLenth = ((int) KeyWord[t]) - 48; //Calculate KeyLenth
+    (t < (n1 - 1)) ? t++ : (t = 0);
+    int NumLetter = (int) Letter; //Calculate EncDigit
     NumLetter += KeyLenth;
     if(NumLetter > 57) NumLetter = (NumLetter % 57) + 47;
     printf("%c", NumLetter);
