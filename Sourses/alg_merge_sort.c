bcf897beb3ed7c02874aaf21ba43bb4df89f62fc
weenchvd <d88744@gmail.com>
2018-07-31 23:52:18 +0300

    UPDH

---
+++ Sourses/alg_merge_sort.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <libcs50-8.1.0/cs50.h>
+
+
+int main(int argc, string argv[])
+{
+    int n = (argc - 1);
+    int massive[n];
+    for(int i = 0; i < n; i++) massive[i] = atoi(argv[i+1]);
+    printf("Unsorted list:\n");
+    for(int i = 0; i < n; i++) printf("%d\t", massive[i]);
+    for(int i = 0;)
+    {
+
+        for(int s = 0; s < n; s++);
+    }
+    printf("\nSorted list:\n");
+    for(int i = 0; i < n; i++) printf("%d\t", massive[i]);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
