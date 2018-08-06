9fa4a905ceacfe78aa2288740a40d1be1f3121e3
weenchvd <d88744@gmail.com>
2018-08-06 21:30:31 +0300

    UPDW

--- Sourses/alg_merge_sort.c
+++ Sourses/alg_merge_sort.c
@@ -1,32 +1,49 @@
 #include <stdio.h>
 #include <libcs50-8.1.0/cs50.h>
 
+int * MergeSort (int massive[], int lenth);
 
 int main(int argc, string argv[])
 {
-    int n = (argc - 1);
-    int massive[n];
-    for(int i = 0; i < n; i++) massive[i] = atoi(argv[i+1]);
-    printf("Unsorted list:\n");
-    for(int i = 0; i < n; i++) printf("%d\t", massive[i]);
-    for(int i = 0;)
+    int q = (argc - 1);
+    int massive[q];
+    int * pntr_sorted_massive;
+    for(int i = 0; i < q; i++)
     {
-
-        for(int s = 0; s < n; s++);
+        massive[i] = atoi(argv[i+1]);
     }
+    printf("Unsorted list:\n");
+    for(int i = 0; i < q; i++)
+    {
+        printf("%d\t", massive[i]);
+    }
+    pntr_sorted_massive = MergeSort(massive, q);
     printf("\nSorted list:\n");
-    for(int i = 0; i < n; i++) printf("%d\t", massive[i]);
-    return 0;
+    for(int i = 0; i < q; i++)
+    {
+        printf("%d\t", massive[i]);
+    }
+    /*if(MergeSort(massive, &massive[0], &massive[q-1]))
+    {
+        printf("\nSorted list:\n");
+        for(int i = 0; i < q; i++) printf("%d\t", massive[i]);
+        return 0;
+    }
+    else
+    {
+        printf("\nSorting failed!\n");
+        return 1;
+    }*/
 }
 
+int * MergeSort (int massive[], int lenth)
+{
+    if(lenth > 1)
+    {
+        int middle = 0, shift = 0, left_half_lenth = 0, rigth_half_lenth = 0;
+        int * left_half = 0, * right_half = 0;
+        ((lenth % 2) == 1) ? (middle = ((lenth - 1) / 2) + 1) : (middle = (lenth / 2) + 1, shift = 1);
 
+    }
 
-
-
-
-
-
-
-
-
-
+}
