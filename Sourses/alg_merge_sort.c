7eb780eb5564f4c5e5ac9ffa400b2bcc9a2d1743
weenchvd <d88744@gmail.com>
2018-08-28 15:22:38 +0300

    UPDW

--- Sourses/alg_merge_sort.c
+++ Sourses/alg_merge_sort.c
@@ -1,43 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int * MergeSort (int *, int);
+int * MergeSort (int *, unsigned int);
 
 int main(int argc, char * argv[])
 {
-    int n = (argc - 1);
-    int massive[n];
-    int * ptr_UnsortedMassive = massive;
+    unsigned int n = (unsigned int) (argc - 1);
+    int * ptr_UnsortedMassive = calloc(n, sizeof(int));
+
     for(int i = 0; i < n; i++)
     {
-        massive[i] = atoi(argv[i+1]);
+        *(ptr_UnsortedMassive+i) = atoi(argv[i+1]);
     }
     printf("Unsorted list:\n");
     for(int i = 0; i < n; i++)
     {
-        printf("%d\t", massive[i]);
+        printf("%d\t", *(ptr_UnsortedMassive+i));
     }
     int * ptr_SortedMassive = MergeSort(ptr_UnsortedMassive, n);
+    free(ptr_UnsortedMassive);
     printf("\nSorted list:\n");
     for(int i = 0; i < n; i++)
     {
         printf("%d\t", *(ptr_SortedMassive+i));
     }
+    free(ptr_SortedMassive);
+    return 0;
 }
 
-int * MergeSort (int * ptr_UnsortedMassive, int n)
+int * MergeSort (int * ptr_UnsortedMassive, unsigned int n)
 {
     if(n == 1)
     {
-        return ptr_UnsortedMassive;
+        int * ptr_SortedMassive = malloc(sizeof(int));
+        *(ptr_SortedMassive) = *(ptr_UnsortedMassive);
+        return ptr_SortedMassive;
     }
     else
     {
-        int n_LeftHalf = 0, n_RightHalf = 0;
-        int SortedMassive[n];
+        unsigned int n_LeftHalf = 0, n_RightHalf = 0;
         int * ptr_SortedLeftHalf = NULL;
         int * ptr_SortedRightHalf = NULL;
-        int * ptr_SortedMassive = SortedMassive;
         if(n % 2)
         {
             n_LeftHalf = (n - 1) / 2;
@@ -52,32 +55,35 @@ int * MergeSort (int * ptr_UnsortedMassive, int n)
             ptr_SortedRightHalf = MergeSort((ptr_UnsortedMassive + n / 2), n_RightHalf);
         }
 
+        int * ptr_SortedMassive = calloc(n, sizeof(int));
         for(int i = 0, x = 0, y = 0; i < n; i++)
         {
             if(x < n_LeftHalf && y < n_RightHalf)
             {
                 if(*(ptr_SortedLeftHalf+x) < *(ptr_SortedRightHalf+y))
                 {
-                    SortedMassive[i] = *(ptr_SortedLeftHalf+x);
+                    *(ptr_SortedMassive+i) = *(ptr_SortedLeftHalf+x);
                     x++;
                 }
                 else
                 {
-                    SortedMassive[i] = *(ptr_SortedRightHalf+y);
+                    *(ptr_SortedMassive+i) = *(ptr_SortedRightHalf+y);
                     y++;
                 }
             }
             else if (x < n_LeftHalf)
             {
-                SortedMassive[i] = *(ptr_SortedLeftHalf+x);
+                *(ptr_SortedMassive+i) = *(ptr_SortedLeftHalf+x);
                 x++;
             }
             else
             {
-                SortedMassive[i] = *(ptr_SortedRightHalf+y);
+                *(ptr_SortedMassive+i) = *(ptr_SortedRightHalf+y);
                 y++;
             }
         }
+        free(ptr_SortedLeftHalf);
+        free(ptr_SortedRightHalf);
         return ptr_SortedMassive;
     }
 }
